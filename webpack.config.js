47b941dbd15ddd17a7fae483f308296ccce45cfe
Md Salim Rana <rana.salim.rana@gmail.com>
2022-01-07 19:19:09 +0600

    Setup typescript and webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/app.ts',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+}
